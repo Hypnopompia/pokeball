8d9f09f8c30dfaaa1cbbdcd5f913cf741ee06303
TJ Hunter <tjhunter@gmail.com>
2016-08-08 20:34:48 -0600

    move logic to model, more logging

--- server/app/Console/Commands/Import.php
+++ server/app/Console/Commands/Import.php
@@ -48,57 +48,7 @@ class Import extends Command
 			return;
 		}
 
-		$lat = $pokeball->latitude;
-		$lon = $pokeball->longitude;
-
-		$tenMinutesAgo = Carbon::now()->subMinutes(10);
-
-		if ($lat == 0 || $lon == 0 || $pokeball->updated_at < $tenMinutesAgo) {
-			return; // Don't know where the pokeball is or it hasn't checked in for a while.
-		}
-
-		$command = "node /home/ubuntu/pogonode/pokemon.js " . $lat . " " . $lon . " 2>&1";
-		$json = shell_exec($command);
-
-		// $json = '[{"encounterId":"6394711798519346173","pokemon":{"id":58,"name":"Growlithe"},"latitude":40.38698761751936,"longitude":-111.82048104150228,"expires":1.171}]';
-
-
-		$encounters = json_decode($json, true);
-		$imported = 0;
-
-		if (is_array($encounters)) {
-			foreach ($encounters as $encounter) {
-				$sighting = Sighting::where('encounterid', $encounter['encounterId'])->first();
-				if (!$sighting) { // New sighting!
-					$sighting = new Sighting;
-					$sighting->encounterid = $encounter['encounterId'];
-					$sighting->pokemon_id = $encounter['pokemon']['id'];
-					$sighting->pokeball_id = $pokeball->id;
-					$sighting->latitude = $encounter['latitude'];
-					$sighting->longitude = $encounter['longitude'];
-					$sighting->expires = Carbon::createFromTimestamp(time() + intval($encounter['expires']));
-					$sighting->save();
-
-					$distance = $sighting->distanceFrom($pokeball);
-
-					Log::info("New " . $sighting->pokemon->name . ' in ' . $distance . ' feet.');
-
-					$imported++;
-				}
-			}
-
-			Log::info($imported . " new sightings.");
-
-			if ($imported > 0) {
-				Log::info("New pokemon found. Go catch em!");
-				$pokeball->wiggle();
-			}
-
-		} else {
-			Log::error("node script failed.");
-			Log::debug($json);
-		}
-
+		$pokeball->scan();
 
 	}
 }
--- server/app/Pokeball.php
+++ server/app/Pokeball.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
+use App\Sighting;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
 use Log;
-use Carbon\Carbon;
 
 class Pokeball extends Model {
 	use SoftDeletes;
@@ -23,6 +23,59 @@ class Pokeball extends Model {
 		'latitude' => 'float',
 		'longitude' => 'float',
 	];
+
+	public function scan() {
+		$tenMinutesAgo = Carbon::now()->subMinutes(10);
+
+		if ($this->latitude == 0 || $this->longitude == 0 || $this->updated_at < $tenMinutesAgo) {
+			return; // Don't know where the pokeball is or it hasn't checked in for a while.
+		}
+
+		$command = "node /home/ubuntu/pogonode/pokemon.js " . $this->latitude . " " . $this->longitude . " 2>&1";
+		Log::debug($command);
+
+		$json = shell_exec($command);
+
+		// $json = '[{"encounterId":"6394711798519346173","pokemon":{"id":58,"name":"Growlithe"},"latitude":40.38698761751936,"longitude":-111.82048104150228,"expires":1.171}]';
+
+
+		$encounters = json_decode($json, true);
+		$imported = 0;
+
+		if (is_array($encounters)) {
+			foreach ($encounters as $encounter) {
+				$sighting = Sighting::where('encounterid', $encounter['encounterId'])->first();
+				if (!$sighting) { // New sighting!
+					$sighting = new Sighting;
+					$sighting->encounterid = $encounter['encounterId'];
+					$sighting->pokemon_id = $encounter['pokemon']['id'];
+					$sighting->pokeball_id = $this->id;
+					$sighting->latitude = $encounter['latitude'];
+					$sighting->longitude = $encounter['longitude'];
+					$sighting->expires = Carbon::createFromTimestamp(time() + intval($encounter['expires']));
+					$sighting->save();
+
+					$distance = $sighting->distanceFrom($this);
+
+					Log::info("New " . $sighting->pokemon->name . ' in ' . $distance . ' feet.');
+
+					$imported++;
+				}
+			}
+
+			Log::info($imported . " new sightings.");
+
+			if ($imported > 0) {
+				Log::info("New pokemon found. Go catch em!");
+				$this->wiggle();
+			}
+
+		} else {
+			Log::error("node script failed.");
+			Log::debug($json);
+		}
+
+	}
 
 	public function wiggle() {
 		Log::info("Sending wiggle command.");
